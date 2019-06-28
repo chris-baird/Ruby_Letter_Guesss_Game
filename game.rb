c390cddbdb84de64ade9fb044597c546fc5c21e1
Chris Baird <chrisbaird05@icloud.com>
2019-06-27 19:57:52 -0700

    Added way to generate random letter to guess

--- game.rb
+++ game.rb
@@ -1,15 +1,17 @@
 puts 'Guess a letter'
 class Game 
-  @@letter = 'C'
+  @@letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T', 'U', 'V','W','X','Y','Z']
+  @@letter = @@letters[rand 26]
   @@guesses = 9
   @@user_guess = gets.chomp.upcase
   def letterGuess
-    puts "You have #{@@guesses} guesses left"
+  puts @@letter
     if @@guesses != 0 
       if @@user_guess == @@letter
         puts 'You Win'
       else
         @@guesses = @@guesses - 1
+        puts "You have #{@@guesses} guesses left"
         @@user_guess = gets.chomp.upcase
         letterGuess
       end 
@@ -22,3 +24,5 @@ end
 
 guessGame = Game.new
 guessGame.letterGuess
+
+
